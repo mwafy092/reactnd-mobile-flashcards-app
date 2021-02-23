0b3bdb8d5c07225caf3bad458b1a4635b6a612ff
mwafy092 <mwafy092@gmail.com>
2021-02-23 20:19:33 +0200

    action button added

---
+++ components/ActionButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+export default function ActionButton({ onPress, styles, text, color }) {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={[styles.iosBtn, { backgroundColor: color }]}>
+      <Text style={styles.submitBtnText}>{text}</Text>
+    </TouchableOpacity>
+  );
+}
--- components/DeckView.js
+++ components/DeckView.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { getData } from '../utils/api';
 import { connect } from 'react-redux';
+import ActionButton from './ActionButton';
+import { purple, white, red } from '../utils/colors';
 class DeckView extends React.Component {
   render() {
     const deck = this.props.route.params.deck;
@@ -9,8 +11,22 @@ class DeckView extends React.Component {
     const { decks } = this.props;
     return (
       <View style={styles.container}>
-        <Text>{decks[deck].title}</Text>
-        <Text>{decks.questions && decks[deck].questions.length}</Text>
+        <Text style={{ fontSize: 30 }}>{decks[deck].title}</Text>
+        <Text style={{ color: 'red', fontSize: 18, margin: 10 }}>
+          {decks.questions ? decks[deck].questions.length : 'empty deck'}
+        </Text>
+        <ActionButton
+          color={purple}
+          styles={styles}
+          text={'Add Card'}
+          onPress={() => this.props.navigation.navigate('AddCard', { deck })}
+        />
+        <ActionButton
+          color={red}
+          styles={styles}
+          text={'Add Card'}
+          onPress={() => this.props.navigation.navigate('Quiz', { deck })}
+        />
       </View>
     );
   }
@@ -21,6 +37,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  iosBtn: {
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    margin: 10,
+    width: 170,
+  },
+  submitBtnText: {
+    color: '#fff',
+    fontSize: 22,
+    textAlign: 'center',
   },
 });
 
