42a139eb1932e1b8a4bc5e4a8c98011cd3190528
mwafy092 <mwafy092@gmail.com>
2021-02-23 22:10:32 +0200

    add styles to app

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, ScrollView } from 'react-native';
 import { getData } from '../utils/api';
 import { getDecks } from '../utils/api';
 import { connect } from 'react-redux';
 import { receiveDecks } from '../actions';
+import { orange } from '../utils/colors';
 class DeckList extends React.Component {
   componentDidMount() {
     getDecks().then((decks) => this.props.receiveAllDecks(decks));
@@ -13,15 +14,16 @@ class DeckList extends React.Component {
     const { decks } = this.props;
     console.log(this.props);
     return (
-      <View style={styles.container}>
+      <ScrollView>
         {Object.keys(decks).map((deck) => {
           const { title, questions } = decks[deck];
 
           return (
-            <View key={title}>
-              <Text>{title}</Text>
-              <Text>{questions.length}</Text>
+            <View key={title} style={styles.card}>
+              <Text style={styles.text}>{title}</Text>
+              <Text style={styles.text}>{questions.length}</Text>
               <Button
+                style={styles.submitBtn}
                 title='show deck'
                 onPress={() =>
                   this.props.navigation.push('DeckView', { deck })
@@ -29,7 +31,7 @@ class DeckList extends React.Component {
             </View>
           );
         })}
-      </View>
+      </ScrollView>
     );
   }
 }
@@ -39,6 +41,22 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  card: {
+    flex: 1,
+    justifyContent: 'center',
+    alignSelf: 'center',
+    margin: 5,
+    backgroundColor: '#DDD',
+    width: 200,
+    height: 100,
+    borderRadius: 10,
+  },
+  text: {
+    fontSize: 25,
+    display: 'flex',
+    textAlign: 'center',
+    color: '#333',
   },
 });
 
