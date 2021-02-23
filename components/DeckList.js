3ff72a44ea1d450879b086938bc2aa97804bf87a
mwafy092 <mwafy092@gmail.com>
2021-02-23 15:42:47 +0200

    DeckList added

---
+++ components/DeckList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { getData } from '../utils/api';
+
+export default class DeckList extends React.Component {
+  render() {
+    const decks = getData();
+
+    return (
+      <View>
+        {Object.keys(decks).map((deck) => {
+          const { title, questions } = decks[deck];
+
+          return (
+            <View key={title}>
+              <Text>{title}</Text>
+              <Text>{questions.length}</Text>
+            </View>
+          );
+        })}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
